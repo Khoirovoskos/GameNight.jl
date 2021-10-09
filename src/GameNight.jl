c4949e22880fbc21c6d0aa5e13998fd2a02fc754
Khoirovoskos <83989311+Khoirovoskos@users.noreply.github.com>
2021-10-09 13:39:50 -0500

    Update GameNight.jl

--- src/GameNight.jl
+++ src/GameNight.jl
@@ -207,7 +207,7 @@ function word_search(time_limit = 0)
   
   # Place letters into grid
   for i in 1:16
-    text!(values[i, 3], position = (values[i, 1] - 2.5, values[i, 2] - 2.5), align = (:center, :center), textsize = 96)
+    text!(values[i, 3], position = (values[i, 1] - 2.5, values[i, 2] - 2.5), align = (:center, :center), textsize = 96, space = :data)
   end			
 			
   display(fig)
